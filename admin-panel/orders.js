3f0c339fabd2ff5a14697a6845e944f951063114
asanmohideen <ajmalaneesha281@gmail.com>
2026-08-07 17:17:39 +0530

    Fix order date and time display for India timezone

--- admin-panel/orders.js
+++ admin-panel/orders.js
@@ -80,7 +80,16 @@ function loadOrders() {
 </select>
 </td>
 
-<td>${order.created_at}</td>
+<td>${new Date(order.created_at).toLocaleString("en-IN", {
+    timeZone: "Asia/Kolkata",
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true
+})}</td>
 
   <td>
 
